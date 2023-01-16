4053d939862ac8b0e18fbfc40b5574e66defd857
Reza Heidari <reza.heidari@amplicade.com>
2023-01-16 14:01:20 +0300

    wireup db and smoke check

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 
+const circulationRepo = require('./repos/circulationRepo');
+const data = require('./circulation.json');
+
 const url = 'mongodb://localhost:27017';
 const dbName = 'circulation';
 
@@ -9,8 +12,9 @@ async function main() {
 
     const admin = client.db(dbName).admin();
 
-    console.log(await admin.serverStatus());
-    console.log(await admin.listDatabases());
+    const results = await circulationRepo.loadData(data);
+
+    console.log(results.insertedCount);
 }
 
 main();
